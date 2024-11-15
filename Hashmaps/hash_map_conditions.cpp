a2ff19b6d216fe057ead158d63c9e3d038d9467f
Md_Ajharul_Islam <167879829+MdAjharulIslam@users.noreply.github.com>
2024-11-16 00:52:34 +0600

    Update hash_map_conditions.cpp

--- Hashmaps/hash_map_conditions.cpp
+++ Hashmaps/hash_map_conditions.cpp
@@ -1,33 +1,31 @@
-#include<iostream>;
+#include<iostream>
 using namespace std;
-int main(){
 
+int main() {
 
-/*condition 1
+    /* Condition 1: Collision Resolution Techniques in Hashing
 
-chaining:
+    Chaining:
+    h(k) = k % m  // Hash function: Map key k to a table of size m
+    Insert like a linked list
+    In chaining, each bucket contains a linked list of keys that hash to the same index.
 
-h(k)=k%m  m=size of table
-insert like a linked list
+    Linear Probing:
+    h'(k) = [h(k) + i] % m
+    In linear probing, if a collision occurs at the hashed index, it searches linearly for the next available position.
 
-linear probing:
-h'(k)=[h(k)+i]%m
+    Quadratic Probing:
+    h'(k) = [h(k) + i^2] % m
+    Quadratic probing resolves collisions by incrementing the index based on the square of the attempt number (i).
 
+    Double Hashing:
+    h1(k) = k % m
+    h2(k) = k % 8
+    h3(k) = [h1(k) + i * h2(k)] % m
+    Double hashing uses two hash functions: if a collision occurs, the second hash function is applied to find a new slot.
 
-Quadratic probing:
-h'(k)=[h(k)+^2]%m
+    */
 
-
-Double hashing:
-h1(k)=k%m
-h2(k)=k%8
-
-h3(k)=[h1(k)+i*h2(k)]%m
-
-
-*/
-
-
-cout<<"this is the some role of hash maping"<<endl;
+    cout << "This is some role of hash mapping" << endl;
     return 0;
 }
